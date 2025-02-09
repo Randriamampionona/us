7463a7e5f9ad3518e337edbf82777ee93974618c
unknown <tojorandria474@gmail.com>
2025-02-09 19:33:37 +0300

    BUG: fix type temporary

--- components/gif-section.tsx
+++ components/gif-section.tsx
@@ -31,23 +31,7 @@ export default function GifSection() {
         {loading ? "Loading..." : "Search"}
       </button>
 
-      <div className="mt-8">
-        {gifs.length === 0 ? (
-          <p>No GIFs found.</p>
-        ) : (
-          <div className="grid grid-cols-3 gap-4">
-            {gifs.map((gif) => (
-              <div key={gif.id} className="bg-gray-200 p-2 rounded">
-                <img
-                  src={gif.media[0].url}
-                  alt={gif.title}
-                  className="w-full h-auto"
-                />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      <pre>{gifs}</pre>
     </div>
   );
 }
