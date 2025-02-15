2c31ee69ee3e9d489a3cc4ac77f7fae9e2da43d7
unknown <tojorandria474@gmail.com>
2025-02-15 18:44:37 +0300

    Mock gif

--- components/gif-section.tsx
+++ components/gif-section.tsx
@@ -1,37 +1,10 @@
-"use client";
-
-import { getchGifs } from "@/action/get-gif";
-import { useState } from "react";
+import Image from "next/image";
+import src from "../app/gifs/3.gif";
 
 export default function GifSection() {
-  const [query, setQuery] = useState("cat");
-  const [gifs, setGifs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const handleSearch = async () => {
-    setLoading(true);
-    try {
-      const gifsData = await getchGifs({ query }); // Call the server action to get GIFs
-      setGifs(gifsData);
-    } catch (error) {
-      console.error("Error fetching GIFs:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  console.log(gifs);
-
   return (
     <div>
-      <button
-        onClick={handleSearch}
-        className="bg-blue-500 text-white p-2 rounded"
-      >
-        {loading ? "Loading..." : "Search"}
-      </button>
-
-      <pre>{gifs}</pre>
+      <Image src={src} width={500} height={500} alt="Picture of the author" />
     </div>
   );
 }
