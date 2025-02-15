927dd37550ab4a27852966302a28b0ff221263f4
unknown <tojorandria474@gmail.com>
2025-02-15 23:16:32 +0300

    chore: add copyright notice

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import GiftButton from "@/components/gift-button";
 import QuotSection from "@/components/quote-section";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex items-center justify-center">
+    <main className="w-screen h-screen flex items-center justify-center flex-col">
       <div>
         <h1 className="text-center text-7xl font-bold">Mamour 💕</h1>
         <QuotSection />
         <GiftButton />
       </div>
+
+      <p className="absolute bottom-2 text-sm text-muted-foreground">
+        By <Link href="https://tooj-rtn.vercel.app/">Tooj Rtn</Link> &copy;{" "}
+        {new Date().getFullYear()}
+      </p>
     </main>
   );
 }
